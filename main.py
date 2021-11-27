f62df955df3a3a55ffab62c6d14d0f0aaf81dff4
raducornea <cornearaduvalentin@gmail.com>
2021-11-27 16:01:04 +0200

    struct - pack + unpack basic understanding

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import server_gui
-import file_system as fs
 
 
 def main():
-    FS = fs.FileSystem()
-    FS.get_current_work_directory()
+    """
+    # Struct - pack & unpack
+    from struct import *
+    
+    
+    packed_data = pack("13s i", b"hello, bro", 252525242)
+    print(packed_data)
 
-    FS.new_file()
-    FS.new_directory()
+    print(calcsize("13si"))
+    # i       - 1i (i needs 4 bytes)
+    # b b b b - 4s
+    # b b b b - 4s
+    # b b b b - 4s
+    # b x x x - 1s
+    # 13s + i -> 20 bytes necessary
 
-    print(FS.get_current_work_directory())
-    FS.set_path(FS.current_path + "\\" + 'Temporary File')
-    print(FS.get_current_work_directory())
-
-    FS.new_file()
-    FS.new_directory()
+    unpacked_date = unpack("13s i", packed_data)
+    print(unpacked_date)
+    """
 
     gui = server_gui.GUI()
     gui.open_application()
@@ -23,25 +29,18 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-
-
-
-
-
-
-
-
-
 """
 Sources:
+https://www.youtube.com/watch?v=y49OUKBCEek&ab_channel=AshaTutorials
+https://www.youtube.com/watch?v=-dErxklW4_4&ab_channel=seanwasereytbe
 https://pythontic.com/modules/socket/udp-client-server-example
 https://docs.python.org/3/library/tkinter.html
 https://docs.python.org/3/library/socket.html
 https://docs.python.org/3/library/os.html
 https://www.geeksforgeeks.org/how-to-use-thread-in-tkinter-python/
 https://github.com/nicolaebotezatu/RC-P/blob/main/Non-blocking%20UDP%20peers/udp_peer.py
+https://levelup.gitconnected.com/method-types-in-python-2c95d46281cd
+https://www.pythonprogramming.in/how-to-access-both-cls-and-self-in-a-method-in-python.html
 https://thispointer.com/how-to-create-a-directory-in-python/
 
 """
