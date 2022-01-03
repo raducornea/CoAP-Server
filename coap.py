f49d911ff402a3c0c3999d3ad1e2bedd61c8cd53
raducornea <cornearaduvalentin@gmail.com>
2022-01-03 12:35:41 +0200

    CoAP class

---
+++ coap.py
@@ -0,0 +1,27 @@
+class CoAP:
+    COAP_PAYLOAD_MARKER = 0xff
+    COAP_VERSION = 1
+
+    # Message types
+    TYPE_CONF = 0
+    TYPE_NON_CONF = 1
+    TYPE_ACK = 2
+    TYPE_RESET = 3
+
+    # Message classes
+    CLASS_METHOD = 0
+    CLASS_SUCCESS = 2
+    CLASS_CLIENT_ERROR = 4
+    CLASS_SERVER_ERROR = 5
+
+    # Method codes
+    CODE_EMPTY = 0
+    CODE_GET = 1
+    CODE_POST = 2
+    CODE_RENAME = 8
+
+    def buf2int(buf):
+        returnVal = 0
+        for i in range(len(buf)):
+            returnVal += buf[i] << (8 * (len(buf) - 1 - i))
+        return returnVal
