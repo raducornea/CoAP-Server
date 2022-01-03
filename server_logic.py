f30805a5a74b9c86d6bdfb16f53a0237a265fd07
raducornea <cornearaduvalentin@gmail.com>
2022-01-03 12:36:22 +0200

    Server Logic

--- server_logic.py
+++ server_logic.py
@@ -12,15 +12,20 @@ class Logic:
     running = False
     receive_thread = None
 
+    # TODO
+    #  - make ip addresses into a list of connected [connected ips]
+    #  - each time a client connects => add it into [connected ips] + remove from list when [disconnected]
+
     @classmethod
     def __init__(cls):
-        cls.ip = "127.0.0.1"  # local ip both client and server have
+        cls.ip = "127.0.0.1"  # local ip both client and server have  # ip-ul clientului  172.20.10.4
         cls.server_port = 2001  # my port
         cls.client_port = 2000  # peer port
         cls.data = None
 
         cls.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        cls.server_socket.bind(("127.0.0.1", cls.server_port))
+        cls.server_socket.bind(("127.0.0.2", cls.server_port))  # asta ar trebui scoasa -> de abia cand primeste mesaj
+        # realizeaza conexiunea cu ip-ul pe care il primeste
 
         cls.running = False
         cls.receive_thread = threading.Thread(target=cls.receive_fct, args=(cls.server_socket,))
@@ -50,7 +55,9 @@ class Logic:
         while True:
             data = cls.data
             if data is not None:
+                # data ar trebui sa fie efectiv tot pachetul cu toate campurile inclusiv payload ca json
                 cls.server_socket.sendto(bytes(data, encoding="ascii"), (cls.ip, int(cls.client_port)))
+                # ar trebui modificat si el, incat sa se adapteze fiecarui ip noi primit din data de la client
                 cls.data = None
             if not cls.running:
                 print("TTT Waiting for the thread to close...")
@@ -70,4 +77,5 @@ class Logic:
             else:
                 data, address = cls.server_socket.recvfrom(1024)
                 print("Received ", str(data), " from ", address)
+                # aici ar trebui sa decodific data, incat sa aflu ip-ul
                 print("Counter = ", counter)
