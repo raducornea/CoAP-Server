11947a35d16ad4e38a8ccb043c8952e117aabc7f
raducornea <cornearaduvalentin@gmail.com>
2022-01-06 13:50:44 +0200

    Added JSON for Server & Client

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
 import server_gui
-import message
-from struct import *
 
 
 def main():
-    """
-    # Struct - pack & unpack
-    from struct import *
-
-
-    packed_data = pack("13s i", b"hello, bro", 252525242)
-    print(packed_data)
-
-    print(calcsize("13si"))
-    # i       - 1i (i needs 4 bytes)
-    # b b b b - 4s
-    # b b b b - 4s
-    # b b b b - 4s
-    # b x x x - 1s
-    # 13s + i -> 20 bytes necessary
-
-    unpacked_date = unpack("13s i", packed_data)
-    print(unpacked_date)
-    """
-    my_message = message.Message()
-    my_message.print_details()
-
-    packed_data = my_message.encode_message()
-    print(packed_data)
-    my_message.decode_message(packed_data)
-
     gui = server_gui.GUI()
     gui.open_application()
 
@@ -51,5 +23,4 @@ https://levelup.gitconnected.com/method-types-in-python-2c95d46281cd
 https://www.pythonprogramming.in/how-to-access-both-cls-and-self-in-a-method-in-python.html
 https://stackoverflow.com/questions/111155/how-do-i-handle-the-window-close-event-in-tkinter
 https://thispointer.com/how-to-create-a-directory-in-python/
-
 """
--- server_logic.py
+++ server_logic.py
@@ -1,6 +1,9 @@
+import json
 import socket
 import threading
 import select
+
+import message
 
 
 class Logic:
@@ -10,6 +13,7 @@ class Logic:
     server_ip = ""  # server ip
     server_port = 0  # server port
     server_socket = None
+    server_message = None
 
     data = None
     running = False
@@ -24,6 +28,7 @@ class Logic:
 
         cls.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         cls.server_socket.bind((cls.server_ip, cls.server_port))
+        cls.server_message = message.Message('Server')
 
         # clients addresses # 172.20.10.4 e.g.
         cls.client_adresses = []
@@ -54,9 +59,6 @@ class Logic:
             return
 
         while True:
-            # todo
-            #  data ar trebui sa fie efectiv tot pachetul
-            #  cu toate campurile inclusiv payload ca json
             if cls.data is not None:
                 cls.message_clients(cls.data)
 
@@ -88,14 +90,32 @@ class Logic:
             else:
                 # server connects to clients as well when they send a message
                 data, address = cls.server_socket.recvfrom(1024)
-                print("Received ", str(data), " from ", address)
+                print("Received", str(data), "from", address)
 
                 # add client address when client sends to server
                 if address not in cls.client_adresses:
                     cls.client_adresses.append(address)
 
-                # remove client address if message is 'Disconnected'
-                if data == b'Disconnected':
-                    cls.client_adresses.remove(address)
+                # process data
+                cls.process_data(data, address)
 
                 print("Counter = ", counter)
+
+    @classmethod
+    def process_data(cls, data, address):
+        # data is of type packed_data
+        """
+        header_format - (81, 38, 255, 255, 0, 255)
+        encoded_json - b'{"command": "hello"}'
+        command - hello
+        """
+        header_format, encoded_json = cls.server_message.decode_message(data)
+        command = json.loads(encoded_json)['command']
+        print(command)
+
+        # verify header format from data and do CoAP Codes
+        cls.server_message.verify_format(header_format, command)
+
+        # remove client address if message is 'Disconnected'
+        if command == 'disconnect':
+            cls.client_adresses.remove(address)
