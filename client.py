dd62bd9d6450fe6ff4e643a5984ef6182af6f4e1
raducornea <cornearaduvalentin@gmail.com>
2022-01-11 17:29:58 +0200

    Client can send CoAP Packets and payloads with JSONS

--- client.py
+++ client.py
@@ -6,17 +6,20 @@ import select
 import message
 
 
+# the function that interprets the Header & Payload received by the server
+
 def receive_fct(s):
     global running
-    contor = 0
+    counter = 0
     while running:
         r, _, _ = select.select([s], [], [], 1)
         if not r:
-            contor = contor + 1
+            counter = counter + 1
         else:
             data, address = s.recvfrom(1024)
-            print("S-a receptionat ", str(data), " de la ", address)
-            print("Contor= ", contor)
+            print("Received", str(data), "from", address)
+            # process_data(data, address)
+            print("Counter = ", counter)
 
 
 def main():
@@ -44,11 +47,59 @@ def main():
 
     while True:
         try:
-            data = input("Trimite: ")  # 'cwd', 'ls'
-            my_message.set_client_payload(data)
-            packed_data = my_message.encode_message()
+            command = input("Command: ")  # 'cwd', 'ls'
+            parameters = input("Parameters: ")  # old_name new_name
+            message_type = int(input("Message Type # 0 conf 1 non-conf 2 ack 3 reset = "))  # int
 
+            """
+            GET - cwd, ls -> response should be 2.05 (Content)
+            The GET method requests a representation of the specified resource. Requests using GET should only 
+            retrieve data.
+
+            POST - newDir, newFile -> response 2.01 (Created)
+            The POST method submits an entity to the specified resource, often causing a change in state or side effects 
+            on the server.
+
+            PUT - move? file and directories? -> 2.04 (Changed)
+            The PUT method replaces all current representations of the target resource with the request payload.
+
+            RENAME - rename -> 2.04 (Changed) Response 
+            Renames file/directory
+
+            4.04 Not Found ??? -> Client
+            This Response Code is like HTTP 404 "Not Found".
+            
+            # 408 Request Timeout response - Server Shutdown
+            # 499 Client Closed Request - Client Shutdown
+
+            5.01 Not Implemented -> Server
+            This Response Code is like HTTP 501 "Not Implemented".
+            """
+            my_message.set_msg_version(0)
+            my_message.set_msg_token_length(1)
+            my_message.set_msg_id(0xffff)
+            my_message.set_token(0)
+            my_message.set_payload_marker(0xff)
+
+            if command in ['cwd', 'ls']:
+                my_message.set_msg_class(2)
+                my_message.set_msg_code(5)
+            elif command in ['newDir', 'newFile']:
+                my_message.set_msg_class(2)
+                my_message.set_msg_code(1)
+            elif command == ['move', 'rename']:
+                my_message.set_msg_class(2)
+                my_message.set_msg_code(4)
+
+            if message_type in [0, 1, 2, 3]:
+                my_message.set_msg_type(message_type)
+            else:
+                my_message.set_msg_type(1)  # non confirmable
+
+            my_message.set_client_payload(command, parameters)
+            packed_data = my_message.encode_message()
             s.sendto(packed_data, (server_ip, int(server_port)))
+
         except KeyboardInterrupt:
             running = False
             print("Waiting for the thread to close...")
